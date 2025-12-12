05c7b948600f866e8997b36ff9ce0d895eecd546
Paul Krimm <pm.krimm@gmail.com>
2025-12-12 00:14:43 -0500

    Added confirmation modal Blazor component that can be reused throughout the app for confirming form actions such as Deleting an entity.

---
+++ src/FootballSimulator.Web/wwwroot/js/bootstrapInterop.js
@@ -0,0 +1,13 @@
+﻿window.bootstrapInterop = {
+    showModal: function (id) {
+        var modal = new bootstrap.Modal(document.querySelector(id));
+        modal.show();
+    },
+    hideModal: function (id) {
+        var modalEl = document.querySelector(id);
+        var modal = bootstrap.Modal.getInstance(modalEl);
+        if (modal) {
+            modal.hide();
+        }
+    }
+};
