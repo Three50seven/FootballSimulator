55976ad24fef5504d70baf28dd7ceecc2e0c3a91
Paul Krimm <pm.krimm@gmail.com>
2025-12-19 14:02:15 -0500

    Added paging component and used on stadiums manage page

---
+++ src/Common.Core/Domain/ValueObjects/Page.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.Core.Domain
+{
+    public class Page
+    {
+        private int _size = PageCriteria.DefaultPageSize;
+
+        public Page()
+        {
+            
+        }
+        public Page(int page, int size)
+        {
+            Number = page;
+            Size = size;
+        }
+        public int Number { get; set; }
+        public int Size
+        {
+            get => _size;
+            set => _size = value > 0 ? value : PageCriteria.DefaultPageSize;
+        }
+
+    }
+}
